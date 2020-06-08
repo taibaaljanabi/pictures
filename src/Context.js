4c289c8e8b4eb5ecedf21a1efc9f292e1ee13d7a
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2020-06-07 21:11:46 -0600

    adding context

---
+++ src/Context.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Context = React.createContext()
+
+function ContextProvider({children}){
+    return (
+        <Context.Provider value = ''>
+            {children}
+        </Context.Provider>
+    )
+}
+export  {ContextProvider, Context}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import {ContextProvider} from './Context'
 
 ReactDOM.render(  
-
-<Router>
-  <App />
-</Router>
+ <ContextProvider>
+    <Router>
+      <App />
+    </Router>
+</ContextProvider>
 
  ,
   document.getElementById('root')
