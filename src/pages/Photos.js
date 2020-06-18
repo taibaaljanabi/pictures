200d4a2ba5961992009606aca12469c2af583c1e
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2020-06-17 23:24:08 -0600

    commit

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {Context} from '../Context'
 
 export default function Photos() {
+  const {allphotos} = useContext(Context)
+   console.log(allphotos)
   return (
     <div>
       <h1>Hello from Photos</h1>
