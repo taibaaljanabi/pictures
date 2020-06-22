dbefd385efb0450e5219e0fd6b39cfb02fce181f
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2020-06-21 22:59:50 -0600

    adding image component

---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Image() {
+    return (
+        <div className={`${className} image-container`}>
+            <img className='image-grid' src={???}/>
+        </div>
+    )
+}
