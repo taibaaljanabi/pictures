a375f3cdce9db7993dccd7378557c10e0184341a
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2020-07-06 23:06:47 -0600

    adding onhover state

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Image({className, img}) {
+    const [hover, setHover] = useState(false)
+    console.log(hover)
     return (
-        <div className={`${className} image-container`}>
+        <div className={`${className} image-container`}
+             onMouseEnter = {() => setHover(true)}
+             onMouseLeave = {() => setHover(false)}
+        >
             <img className='image-grid' src={img.url}/>
         </div>
     )
