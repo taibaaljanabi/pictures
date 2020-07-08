e25877c9fbe111d4b2a4a7859c9bf422ab715b3c
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2020-07-07 21:08:32 -0600

    adding heart and cart icon

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,13 +2,16 @@ import React, {useState} from 'react'
 
 export default function Image({className, img}) {
     const [hover, setHover] = useState(false)
-    console.log(hover)
+    const heartIcon = hover && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hover && <i className="ri-add-circle-line cart"></i>
     return (
         <div className={`${className} image-container`}
              onMouseEnter = {() => setHover(true)}
              onMouseLeave = {() => setHover(false)}
         >
             <img className='image-grid' src={img.url}/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
